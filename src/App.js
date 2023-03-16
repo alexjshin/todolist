6dd3c7b11ed1446beb95078ca15cd01773e9a3f7
Alexander Shin <alexandershin@MacBook-Pro-108.local>
2023-03-15 21:25:38 -0400

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -54,7 +54,6 @@ function App(props) {
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
-        role="list"
         className="ToDo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
